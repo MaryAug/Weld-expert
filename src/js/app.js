5dfd8b52f66b7c1e70f02de596dfdf57de42ee22
MaryAug <martmary566@gmail.com>
2025-03-11 11:28:45 +0300

    footer

---
+++ src/js/app.js
@@ -0,0 +1,95 @@
+import * as flsFunctions from "./modules/functions.js";
+import { Splide } from "@splidejs/splide";
+
+import IMask from "imask";
+import fslightbox from "fslightbox";
+
+flsFunctions.isWebp();
+// flsFunctions.mainOffset();
+
+// const burgerBtn = document.querySelector(".menu-trigger");
+// const menu = document.querySelector(".menu");
+// burgerBtn.addEventListener("click", (e) => {
+//   burgerBtn.classList.toggle("menu-trigger--is-open");
+//   menu.classList.toggle("menu--is-open");
+//   document.body.classList.toggle("locked");
+
+//   menu.addEventListener("click", (e) => {
+//     console.log(e.target);
+//     if (e.target == menu || e.target.classList.contains("menu__link")) {
+//       burgerBtn.classList.remove("menu-trigger--is-open");
+//       menu.classList.remove("menu--is-open");
+//       document.body.classList.remove("locked");
+//     }
+//   });
+// });
+
+//banner-slider
+document.addEventListener("DOMContentLoaded", function () {
+  const bannerSlider = new Splide(".banner-slider__slider", {
+    type: "loop",
+    arrows: true,
+    pagination: false,
+    perPage: 1,
+    // gap: "20px",
+    classes: {
+      arrows: "splide__arrows banner-slider__arrows",
+      arrow: "splide__arrow banner-slider__arrow",
+      prev: "splide__arrow--prev banner-slider__arrow--prev",
+      next: "splide__arrow--next banner-slider__arrow--next",
+    },
+  });
+  bannerSlider.mount();
+});
+
+// const elements = document.querySelectorAll(".form__input--tel");
+// const maskOptions = {
+//   mask: "+{7} (000) 000-00-00",
+// };
+// elements.forEach((element) => {
+//   const mask = IMask(element, maskOptions);
+// });
+
+// const fileInputs = document.querySelectorAll(".form__file-input");
+// fileInputs.forEach((input) => {
+//   input.addEventListener("change", function (e) {
+//     let fileName = this.value.split("\\").pop();
+//     this.nextElementSibling.innerHTML = fileName;
+//   });
+// });
+
+document.querySelector(".current-year").innerHTML = new Date().getFullYear();
+
+// scroll to top
+// if (document.querySelector(".scroll-to-top") != null) {
+//   const scrollBtn = document.querySelector(".scroll-to-top");
+//   window.addEventListener("scroll", () => {
+//     if (window.pageYOffset > 200) {
+//       scrollBtn.style.visibility = "visible";
+//       scrollBtn.style.opacity = "1";
+//     } else {
+//       scrollBtn.style.visibility = "hidden";
+//       scrollBtn.style.opacity = "0";
+//     }
+//   });
+//   scrollBtn.addEventListener("click", () => {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: "smooth",
+//     });
+//   });
+// }
+
+// if (document.querySelector(".tabs") != null) {
+//   flsFunctions.tabs("tabs__btn", "tabs__item", "active");
+// }
+
+flsFunctions.accordion();
+
+// flsFunctions.bindModal(
+//   ".open-modal-callback",
+//   ".modal--callback",
+//   ".modal__close"
+// );
+
+// flsFunctions.bindModal(".open-modal-order", ".modal--order", ".modal__close");
