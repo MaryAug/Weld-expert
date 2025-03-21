6860a66d98265e936ccffcacc5158adb0452baab
Mary <martmary566@gmail.com>
2025-03-21 21:46:40 +0300

    Delete src directory

--- src/components/phonemask/phonemask.js
+++
@@ -1,8 +0,0 @@
-// phone mask
-const elements = document.querySelectorAll('input-selector');
-const maskOptions = {
-  mask: '+{7}(000)000-00-00',
-};
-elements.forEach((element) => {
-  const mask = IMask(element, maskOptions);
-});
